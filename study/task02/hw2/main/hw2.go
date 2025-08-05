1e022133a342c343cf0b353db80d31019a4344d4
1349316513lx-web <1349316513lx@gmail.com>
2025-08-05 21:04:37 +0800

    homework 指针

---
+++ study/task02/hw2/main/hw2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/*
+*
+实现一个函数，接收一个整数切片的指针，将切片中的每个元素乘以2。
+*/
+func inc(data *[]int) {
+	for i := 0; i < len(*data); i++ {
+		(*data)[i] *= 2
+	}
+}
+
+func main() {
+	i := []int{1, 2, 3, 4, 5}
+	fmt.Printf("%p\n", &i)
+	inc(&i)
+	fmt.Println(i)
+}
