fcb668181fe66b69d2044f296c7f365369596fa9
1349316513lx-web <1349316513lx@gmail.com>
2025-08-07 17:01:57 +0800

    0807

--- study/task03/demo1/main/demo1.go
+++ study/task03/demo1/main/demo1.go
@@ -15,24 +15,24 @@ type Product struct {
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("../test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("study/task03/test.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
 	db.AutoMigrate(&Product{})
-	//pdts := []Product{
-	//	{Code: "2", Price: 200},
-	//	{Code: "3", Price: 100},
-	//	{Code: "4", Price: 500},
-	//}
+	pdts := []Product{
+		{Code: "2", Price: 200},
+		{Code: "3", Price: 100},
+		{Code: "4", Price: 500},
+	}
 	//Create
-	//err = gorm.G[Product](db).Create(ctx, &Product{Code: "1", Price: 100})
-	//if err != nil {
-	//	return
-	//}
+	err = gorm.G[Product](db).Create(ctx, &Product{Code: "1", Price: 100})
+	if err != nil {
+		return
+	}
 	//insert batch
-	//err = gorm.G[Product](db).CreateInBatches(ctx, &pdts, len(pdts))
+	err = gorm.G[Product](db).CreateInBatches(ctx, &pdts, len(pdts))
 	//Read
 	product1, err1 := gorm.G[Product](db).Where("id=?", 3).First(ctx)
 	if err1 != nil {
